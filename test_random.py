16024ce9faefd83810964d77c37c6153c7aab4b6
Nate Lowry <nathaniel.lowry@maine.edu>
2024-11-03 17:33:59 -0500

    pushing tests

---
+++ test_random.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 18:05:14 2024
+
+@author: bac0n
+"""
+import pandas as pd
+import numpy as np
+
+batting_splits_real_by_type = pd.read_csv("irl_splits_2015-2024_by_hit_type.csv")
